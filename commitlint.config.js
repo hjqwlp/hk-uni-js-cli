6561e7118ea5991c615ed16bb0d9f44748868ce4
黄立 <2467271537@qq.com>
2022-03-22 14:50:04 +0800

    feat: 验证配置是否正确

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,48 +1,6 @@
-/*
- * @Descripttion: git提交 commit控制
- * @version: 1.0.0
- * @Author: huangli
- * @Date: 2022-02-15 16:30:54
- * @LastEditors: huangli
- * @LastEditTime: 2022-03-16 17:35:27
- */
 module.exports = {
   ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^(\w*|[\u4e00-\u9fa5]*)(?:[\(\（](.*)[\)\）])?[\:\：] (.*)/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-      referenceActions: [
-        'init', // 初始化
-        'feat', // 新功能(feature)
-        'fix', // 修补bug
-        'docs', // 文档(documentation)
-        'style', // 格式、样式(不影响代码运行的变动)
-        'refactor', // 重构(即不是新增功能，也不是修改BUG的代码)
-        'perf', // 优化相关，比如提升性能、体验
-        'test', // 添加测试
-        'build', // 编译相关的修改，对项目构建或者依赖的改动
-        'ci', // 持续集成修改
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // 回滚到上一个版本
-        'workflow', // 工作流改进
-        'mod', // 不确定分类的修改
-        'wip', // 开发中
-        'types', // 类型修改
-        'release', // 版本发布
-      ],
-      issuePrefixes: ['#'],
-      noteKeywords: ['BREAKING CHANGE', '不兼容变更'],
-      fieldPattern: /^-(.*?)-$/,
-      revertPattern: /^Revert\s"([\s\S]*)"\s*This reverts commit (\w*)\./,
-      revertCorrespondence: ['header', 'hash'],
-      warn() {},
-      mergePattern: null,
-      mergeCorrespondence: null,
-    },
-  },
   rules: {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [1, 'always'],
@@ -54,23 +12,21 @@ module.exports = {
       2,
       'always',
       [
-        'init', // 初始化
-        'feat', // 新功能(feature)
-        'fix', // 修补bug
-        'docs', // 文档(documentation)
-        'style', // 格式、样式(不影响代码运行的变动)
-        'refactor', // 重构(即不是新增功能，也不是修改BUG的代码)
-        'perf', // 优化相关，比如提升性能、体验
-        'test', // 添加测试
-        'build', // 编译相关的修改，对项目构建或者依赖的改动
-        'ci', // 持续集成修改
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // 回滚到上一个版本
-        'workflow', // 工作流改进
-        'mod', // 不确定分类的修改
-        'wip', // 开发中
-        'types', // 类型修改
-        'release', // 版本发布
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+        'release',
       ],
     ],
   },
